2a2efc6b73554eaee56f6d6bec812fc2248620f2
Lisa Bost <bost.lisam@gmail.com>
2022-11-07 13:22:57 -0600

    Add project files.

---
+++ FlyoutApp/FlyoutApp/Views/BMICalculator.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlyoutApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BMICalculator : ContentPage
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+
+        void Bmi_Btn_Clicked(object sender, System.EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Inp_height.Text) || String.IsNullOrEmpty(Inp_weight.Text))
+            {
+                Lbl_BMI.Text = "You must enter both height and weight to calculate your BMI.";
+            }
+            else
+            {
+                int height = Int32.Parse(Inp_height.Text);
+                double weight = Int32.Parse(Inp_weight.Text);
+
+                double bmi = 703 * (weight / (height * height));
+
+                Lbl_BMI.Text = $"Your BMI is {bmi}.";
+                Inp_height.Text = "";
+                Inp_weight.Text = "";
+            }
+        }
+    }
+}
---
+++ FlyoutApp/FlyoutApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FlyoutApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FlyoutApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
